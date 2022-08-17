fc081c78646d7c21051b8370f74f7ddd5cfa0731
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-17 14:03:37 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'Moled';
+
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'koko';
+    return leastFavoriteCustomer;
+}
